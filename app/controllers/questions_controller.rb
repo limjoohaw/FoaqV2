30ee53c02f7a4cad3d077e59daac2793b85ede5e
limjoohaw <joohaw.lim@gmail.com>
2018-04-26 19:10:31 +0800

    Done install boostrap and Question controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+require "link_thumbnailer"
+class QuestionsController < ApplicationController
+	
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@questions = Question.all
+		@websites = @questions.map { |q| LinkThumbnailer.generate(q.source_url) }
+	end
+
+	def new
+    @question = Question.new
+	end
+
+	def create
+    @question = current_user.questions.new(question_params)
+			if @question.save
+      	redirect_to question_path(@question)
+      else
+      	flash[:alert] = @question.errors.full_messages.join(',')
+      	render :new
+      end
+    # @question.user_id = current_user.id
+	end
+
+	def show
+		@question = Question.find(params[:id])
+		@website = LinkThumbnailer.generate(@question.source_url)
+	end	
+
+
+	private
+
+	def question_params
+	    params.require(:question).permit(:title, :description, :source_url, :linkthumbnailer)
+	end
+
+end
+
+
+
+# class QuestionsController < ApplicationController
+	# def index
+	# 	@questions = Question.all
+	# 	@websites = @questions.map { |q| LinkThumbnailer.generate(q.my_link)}
+	# end
+
+	# def new
+	# 	@question = Question.new
+	# end
+
+	# def create
+	# 	@question = Question.new(allowed_params)
+	# 	@question.save
+	# 	redirect_to question_path(@question)
+	# end
+	
+	# def show
+	# 	@question = Question.find(params[:id])
+	# 	@preview = LinkThumbnailer.generate(@question.my_link)
+	# end	
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+	# def show
+	#   @user = User.find(params[:id])
+	#   @user_questions = @user.questions
+	#   @question = Question.new
+	# end
+
+	# def create
+	#   @user = User.find(params[:id])
+
+	#   @question = Question.new(question_params) 
+	#   @question.receiver = @user.id #This does not work 
+	#   if @question.save
+	#       redirect_to root_path
+	#   else
+	#       render 'new'
+	#   end
+	#  end
+
+	# private
+
+	# def question_params
+	#     params.require(:question).permit(:content, :user_id, :receiver)
+	# end
+
+end
