98ded47ea9b9524135ccc2f0b59cefd3f1746e6b
limjoohaw <joohaw.lim@gmail.com>
2018-04-27 12:58:06 +0800

    Done question index and show error and merge answer controller view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require trix
 //= require_tree .
 //= require jquery3
 //= require popper
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
 	has_one :answer
   belongs_to :user#, optional: true
+
+  attr_accessor :website
 end
