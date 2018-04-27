30bc40458e7706a800239e360016d02ff1dedc5e
limjoohaw <joohaw.lim@gmail.com>
2018-04-27 18:42:22 +0800

    Done answer reply on question and basic some authorization on question#show

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,22 @@
 class AnswersController < ApplicationController
 
-	def index
-		@answers = Answer.all
-	end
+	# def index
+	# 	@answers = Answer.all
+	# end
 
-	def new
-		@answer = Answer.new
-	end
+	# def new
+	# 	@answer = Answer.new
+	# 	@answer = Question.answer.new(allowed_params)
+	# 	@answer.question_id = params[:question_id]
+	# end
 
 	def create
-		@answer = Answer.new(allowed_params)
+		@answer = Answer.new
+		@answer = current_user.answers.new(allowed_params)
+		@question = Question.find(params[:question_id])
+		@answer.question_id = params[:question_id]
 		if @answer.save
+			redirect_to question_path(@question)
     else
     	flash[:alert] = @answer.errors.full_messages.join(',')
     	render :new
@@ -21,22 +27,22 @@ class AnswersController < ApplicationController
 		# end
 	end
 
-	def edit
-		@answer = Answer.find(params[:id])
-	end
+	# def edit
+	# 	@answer = Answer.find(params[:id])
+	# end
 
-	def update
-		@answer = Answer.find(params[:id])
-		if @answer.update_attributes(allowed_params)
-			redirect_to answers_path
-		else
-			redender 'new'
-		end
-	end
+	# def update
+	# 	@answer = Answer.find(params[:id])
+	# 	if @answer.update_attributes(allowed_params)
+	# 		redirect_to answers_path
+	# 	else
+	# 		redender 'new'
+	# 	end
+	# end
 
-	def show
-		@answer = Answer.find(params[:id])
-	end
+	# def show
+		# @answer = Answer.find(params[:question_id])
+	# end
 
 
 	def destroy
